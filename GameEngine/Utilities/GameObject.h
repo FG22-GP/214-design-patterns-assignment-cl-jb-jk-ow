8c5e221390279de37caa0b34fda6330d44971571
joebinns <hello@joebinns.com>
2023-12-01 14:09:55 +0100

    Add transform to game object

--- GameEngine/Utilities/GameObject.h
+++ GameEngine/Utilities/GameObject.h
@@ -1,6 +1,17 @@
 #pragma once
 
+struct Position {
+public:
+    int x;
+    int y;
+};
+
+struct Transform {
+public:
+    Position position;
+};
+
 class GameObject {
 public:
-    
+    Transform transform;
 };
