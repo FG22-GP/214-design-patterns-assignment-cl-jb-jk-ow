ce6020660d0c4865a2b304edbc482a82d5a7c67e
joebinns <hello@joebinns.com>
2023-12-05 11:07:06 +0100

    Fix static vector error and add enable and disable to gameobject class

--- GameEngine/Utilities/GameObject.cpp
+++ GameEngine/Utilities/GameObject.cpp
@@ -1,7 +1,24 @@
 #include "GameObject.h"
 
-GameObject::GameObject(Transform transform)
-{
-	GameObjects.push_back(this);
+GameObject::GameObject(Transform transform){
 	CurrentTransform = transform;
 }
+
+void GameObject::Enable(){
+	ActiveGameObjects.push_back(this);
+}
+
+void GameObject::Disable(){
+	std::erase_if(ActiveGameObjects, [this](GameObject* gameObject) {
+		return gameObject == this;
+		});
+}
+
+/*
+void GameObject::Update(){
+	for (GameObject* gameObject : GameObjects)
+	{
+		gameObject.
+	}
+}
+*/
